c4fa9be5fe8bb99024de03005dc7fde73c514953
Vinay Agarwal <vinay19100@gmail.com>
2023-03-05 18:36:07 +0530

    Adding deepcopy files

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -19,15 +19,15 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator@v0.26.2 2>/dev/null || echo ../code-generator)}
 
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 "${CODEGEN_PKG}/generate-groups.sh" "deepcopy,client,informer,lister" \
-  k8s.io/sample-controller/pkg/generated \
-  k8s.io/sample-controller/pkg/apis \
+  k8s.io/sample-controller@v0.26.1/pkg/generated \
+  k8s.io/sample-controller@v0.26.1/pkg/apis \
   samplecontroller:v1alpha1 \
   --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
